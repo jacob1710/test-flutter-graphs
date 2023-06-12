9899675886a13ee3cc255b431e3f316d463e48ee
Jacob Scase <jacob.scase@checkd.media>
2023-06-12 13:24:03 +0100

    Fix fl chart sizing

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
+Color get kFlashPicksBlue => const Color.fromRGBO(5, 47, 107, 1);
+charts.Color get kChartsFlashPicksBlue => const charts.Color(r: 5, g: 47, b: 107);
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -38,6 +41,8 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
+bool showPreviousLines = false;
 
 class _MyHomePageState extends State<MyHomePage> {
 
@@ -51,6 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
   double graphNeedsChangeCount = 0;
   double secondGraphNeedsChangeCount = 0;
   int itemsInList = 1;
+  Widget googleChart = SizedBox.shrink();
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
         iconSize:50,
         onPressed: () {
           setState(() {
-            itemsInList = 1+Random().nextInt(6);
+            itemsInList = 1+Random().nextInt(12);
             print(itemsInList);
             valuesDouble = List.generate(itemsInList, (index) {
               return Random().nextDouble()*maxVal;
@@ -83,6 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
             print(valuesDouble);
             graphNeedsChangeCount ++;
             secondGraphNeedsChangeCount ++;
+            updateGoogleChart();
           });
         },
         icon: Icon(Icons.refresh_rounded),
@@ -92,52 +99,38 @@ class _MyHomePageState extends State<MyHomePage> {
           // height: 300,
           child: Column(
             children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("Prev. Lines"),
+                  Switch.adaptive(
+                      value: showPreviousLines,
+                      onChanged: (newVal){
+                        setState(() {
+                          showPreviousLines = newVal;
+                          updateGoogleChart();
+                        });
+                      }
+                  )
+                ],
+              ),
               Container(
                 height: 300,
                   child: get2()
               ),
-              SizedBox(
-                height: 300,
-                width: 300,
-                child: charts.OrdinalComboChart(
-                    _createSampleData(),
-                  animate: true,
-                  animationDuration: Duration(milliseconds: 500),
-                  behaviors: [
-                    charts.LinePointHighlighter(
-                      symbolRenderer: charts.CircleSymbolRenderer(
 
-                      ),
-                    )
-                  ],
-                  // Configure the default renderer as a bar renderer.
-                  defaultRenderer: charts.BarRendererConfig(
-                      groupingType: charts.BarGroupingType.grouped),
-                  // Custom renderer configuration for the line series. This will be used for
-                  // any series that does not define a rendererIdKey.
-                  customSeriesRenderers: [
-                    charts.LineRendererConfig(
-                        symbolRenderer: charts.RectSymbolRenderer(
 
-                        ),
-                      includePoints: true,
-                      stacked: false,
-                      // ID used to link series to this renderer.
-                        customRendererId: 'customLine'),
-                    charts.LineRendererConfig(
-                        symbolRenderer: charts.CircleSymbolRenderer(
 
-                        ),
-                        // ID used to link series to this renderer.
-                        customRendererId: 'straightLine'),
-                    // charts.LineRendererConfig(
-                    //     // ID used to link series to this renderer.
-                    //     customRendererId: 'straightLine'),
-                    // charts.LineRendererConfig(
-                    //
-                    // )
-                  ]),
-              ),
+
+              // SizedBox(
+              //   height: 300, width: 300,
+              //   child: googleChart
+              // ),
+              //
+
+
+
+
               // Chart(
               //   state: ChartState<void>(
               //       data: ChartData.fromList(
@@ -212,15 +205,82 @@ class _MyHomePageState extends State<MyHomePage> {
       )
     );
   }
+
+  void updateGoogleChart() async {
+    setState(() {
+      googleChart = SizedBox.shrink();
+    });
+    await Future.delayed(Duration(milliseconds: 300));
+    setState(() {
+        googleChart = charts.OrdinalComboChart(
+            _createSampleData(),
+            animate: true,
+            animationDuration: Duration(milliseconds: 500),
+            behaviors: [
+              charts.LinePointHighlighter(
+                symbolRenderer: charts.CircleSymbolRenderer(
+
+                ),
+              )
+            ],
+            // Configure the default renderer as a bar renderer.
+            defaultRenderer: charts.BarRendererConfig(
+                groupingType: charts.BarGroupingType.grouped),
+            // Custom renderer configuration for the line series. This will be used for
+            // any series that does not define a rendererIdKey.
+            customSeriesRenderers: [
+              if(showPreviousLines)
+              charts.LineRendererConfig(
+                  symbolRenderer: charts.RectSymbolRenderer(
+
+                  ),
+                  includePoints: true,
+                  stacked: false,
+                  // ID used to link series to this renderer.
+                  customRendererId: 'customLine'),
+              charts.LineRendererConfig(
+                  symbolRenderer: charts.CircleSymbolRenderer(
+
+                  ),
+                  // ID used to link series to this renderer.
+                  customRendererId: 'straightLine'),
+              // charts.LineRendererConfig(
+              //     // ID used to link series to this renderer.
+              //     customRendererId: 'straightLine'),
+              // charts.LineRendererConfig(
+              //
+              // )
+            ]
+        );
+    });
+
+  }
+
   Widget get2(){
-    double width = 30;
+
+    // bar was 34 * length+1
+    // line was 35.2 * length+1
+
+
+    double containerWidth = 350;
+    double barTotalWidth = containerWidth - 40;
+    // double width = values.length
+    double barWidth = ((barTotalWidth) / (values.length)) - 5;
+
+    double lineWidth = containerWidth;
+
+    double minY = 10;
+
+
     return Stack(
       children: [
-        Container(
-          width: ((width+4)*(values.length+1)),
+        SizedBox(
+          width: containerWidth,
+          // width: ((width+4)*(values.length+1)),
           // key: ValueKey(secondGraphNeedsChangeCount),
           child: BarChart(
               BarChartData(
+                minY: minY,
                 maxY: 100,
                   groupsSpace: 5,
                   alignment: BarChartAlignment.start,
@@ -246,12 +306,24 @@ class _MyHomePageState extends State<MyHomePage> {
                       )
                   ),
                   borderData: FlBorderData(
-                      show: false
+                      show: true
                   ),
-                  extraLinesData: ExtraLinesData(
+                  extraLinesData: showPreviousLines? null : ExtraLinesData(
                       extraLinesOnTop: true,
                       horizontalLines: [
-                        HorizontalLine(y: values.average, label: HorizontalLineLabel(show: true), color: Colors.yellow)
+                        HorizontalLine(
+                            y: values.average,
+                            label: HorizontalLineLabel(
+                                show: true,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  background: Paint()..color = kFlashPicksBlue
+                                    ..strokeWidth = 17
+                                    ..style = PaintingStyle.stroke,
+                                )
+                            ),
+                            color: kFlashPicksBlue
+                        )
                       ]
                   ),
                   barGroups: List.generate(values.length, (index){
@@ -260,10 +332,10 @@ class _MyHomePageState extends State<MyHomePage> {
                         barRods: [
                           BarChartRodData(
                               fromY: 0,
-                              width: width,
+                              width: barWidth,
                               borderRadius: BorderRadius.zero,
                               toY: values[index].toDouble(),
-                              color: values[index] >= baseline?Colors.green:Colors.red
+                              color: values[index] >= values.average?Colors.green:Colors.red
                           )
                         ]
                     );
@@ -284,19 +356,39 @@ class _MyHomePageState extends State<MyHomePage> {
             swapAnimationDuration: Duration(seconds: 1),
           ),
         ),
+        if(showPreviousLines)
         Container(
           alignment: Alignment.centerLeft,
           // height: 150,
-          width: ((width+6)*(values.length+1)),
+          width: lineWidth,
+          // width: ((width+5.2)*(values.length+1)),
           // key: ValueKey(graphNeedsChangeCount),
           // width: 200,
           child: LineChart(
               LineChartData(
                   maxX: values.length.toDouble() - 0.5,
                   minX: -0.45,
-                  minY: 0,
+                  minY: minY,
                   maxY: 100,
                   borderData: FlBorderData(show: false),
+                  extraLinesData: ExtraLinesData(
+                      extraLinesOnTop: true,
+                      horizontalLines: [
+                        HorizontalLine(
+                            y: values.average,
+                            label: HorizontalLineLabel(
+                                show: true,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  background: Paint()..color = kFlashPicksBlue
+                                    ..strokeWidth = 17
+                                    ..style = PaintingStyle.stroke,
+                                )
+                            ),
+                            color: kFlashPicksBlue
+                        )
+                      ]
+                  ),
                   titlesData: FlTitlesData(
                       show: true,
                       topTitles: AxisTitles(
@@ -320,8 +412,10 @@ class _MyHomePageState extends State<MyHomePage> {
                           )
                       )
                   ),
+                  clipData: FlClipData.all(),
                   lineBarsData: [
                     LineChartBarData(
+                        color: Colors.grey,
                         dotData: FlDotData(
 
                         ),
@@ -341,7 +435,7 @@ class _MyHomePageState extends State<MyHomePage> {
         Visibility(
           visible: false,
           child: Container(
-            width: ((width+4)*(values.length+1)),
+            width: containerWidth,
             // key: ValueKey(secondGraphNeedsChangeCount),
             child: BarChart(
                 BarChartData(
@@ -384,7 +478,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           barRods: [
                             BarChartRodData(
                                 fromY: 0,
-                                width: width,
+                                width: barWidth,
                                 borderRadius: BorderRadius.zero,
                                 toY: values[index].toDouble(),
                                 color: Colors.transparent
@@ -441,14 +535,14 @@ List<charts.Series<OrdinalSales, String>> _createSampleData() {
   List<OrdinalSales> data = [];
   valuesDouble.forEachIndexed((index, element) {
     data.add(
-      OrdinalSales(strVals[index], element)
+      OrdinalSales(getTitle(index), element)
     );
   });
 
   List<OrdinalSales> dataLine = [];
   valuesDouble2.forEachIndexed((index, element) {
     dataLine.add(
-        OrdinalSales(strVals[index], element)
+        OrdinalSales(getTitle(index), element)
     );
   });
 
@@ -465,9 +559,10 @@ List<charts.Series<OrdinalSales, String>> _createSampleData() {
         domainFn: (OrdinalSales sales, _) => sales.year,
         measureFn: (OrdinalSales sales, _) => sales.sales,
         data: data),
+    if(showPreviousLines)
     charts.Series<OrdinalSales, String>(
         id: 'customLine',
-        colorFn: (_, __) => charts.MaterialPalette.yellow.shadeDefault,
+        colorFn: (_, __) => charts.MaterialPalette.gray.shadeDefault,
         domainFn: (OrdinalSales sales, _) => sales.year,
         measureFn: (OrdinalSales sales, _) => sales.sales,
         data: dataLine)
@@ -475,7 +570,7 @@ List<charts.Series<OrdinalSales, String>> _createSampleData() {
       ..setAttribute(charts.rendererIdKey, 'customLine'),
     charts.Series<OrdinalSales, String>(
         id: 'straightLine',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        colorFn: (_, __) => kChartsFlashPicksBlue,
         domainFn: (OrdinalSales sales, _) => sales.year,
         measureFn: (OrdinalSales sales, _) => valuesDouble.average,
         data: data)
@@ -666,13 +761,13 @@ Widget getTitles(double value, TitleMeta meta) {
   const style = TextStyle(
     color: Colors.black,
     fontWeight: FontWeight.bold,
-    fontSize: 14,
+    fontSize: 10,
   );
   var val = value.toInt();
   return SideTitleWidget(
     axisSide: meta.axisSide,
-    space: 16,
-    child: Text(strVals[val], style: style),
+    space: 8,
+    child: Text(getTitle(val), style: style),
   );
 }
 
@@ -695,7 +790,15 @@ List<double> valuesDouble= [
 List<double> valuesDouble2 = [
   20.0,10.0,44.0,32.0,32.0,50.1,50.1
 ];
-List<String> strVals = [
+
+String getTitle(int index){
+  if(index < _strVals.length){
+    return _strVals[index];
+  }else{
+    return "";
+  }
+}
+List<String> _strVals = [
   "SEA","DEN","LIV","JAX","CIN","LVR","LVR"
 ];
 
